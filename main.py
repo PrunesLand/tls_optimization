c281b4c8c3c99bce08116689ec830fd09de23378
PrunesLand <panargya@gmail.com>
2026-02-28 17:17:13 +0100

    chore: start working on selection

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from src.genetic_algorithm.fitness_evaluation import evaluate_population
 from src.genetic_algorithm.initialization import generate_population
 
 def genetic_algorithm():
+    population = []
     population = generate_population(BASELINE_TRAFFIC_DATA, OUTPUT_JSON_PATH)
     evaluate_population(population)
+    for generation in range(GENERATIONS):
+        print(f"Generation {generation + 1}/{GENERATIONS}")
+        
+        evaluate_population(population)
+
 
 if __name__ == "__main__":
     genetic_algorithm()
